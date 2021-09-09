895ddb1d0b4ef4b98b1c27efb01d4fd1b3534e88
nate2411 <nathandj28@gmail.com>
2021-09-09 16:18:23 +0200

    update

--- cart.js
+++ cart.js
@@ -1,12 +1,12 @@
 function addToCart(id) {
-    fetch("https://end-of-term.herokuapp.com/show-products/view-product/")
+    fetch("https://end-of-term.herokuapp.com/view-products/")
       .then((response) => response.json())
       .then((data) => {
         console.log(data.data);
         let products = data.data;
   
         let product = products.filter((product) => product[0] == id);
-        let cart_items = JSON.parse(localStorage.getItem("cart"));
+        let cart_items = JSON.parse(localStorage.getItem(".product"));
         console.log(product);
   
         if (cart_items == null) {
@@ -14,12 +14,12 @@ function addToCart(id) {
         }
   
         cart_items.push(product);
-        localStorage.setItem("cart", JSON.stringify(cart_items));
+        localStorage.setItem(".product", JSON.stringify(cart_items));
       });
   }
 
   function getCart() {
-    let cart = JSON.parse(localStorage.getItem("cart")) || [];
+    let cart = JSON.parse(localStorage.getItem(".products")) || [];
     let container = document.querySelector(".book-container");
   
     console.log(cart);
@@ -32,22 +32,25 @@ function addToCart(id) {
     //   let detail = item[0];
   
       container.innerHTML += `
-        <div class="book-container">
-        <div class="images"> <img class="image" src="./images/horror.jpg" alt="horror">
-        <h4 class="name">${book.name}</h4>
-        <h4 class="price">${book.price}</h4>
-        <h4 class="format">${book.format}</h4>
-        <button onclick="removeFromCart(${book.book_id})" class="icons"><i class="far fa-trash-alt">remove</i></button>
-
-        </div>
+      <div class="product">
+            <img src="${card[2]}" alt="" />
+            <div class="product-info">
+              <h3 class="product-name">${card[1]}</h3>
+              <h2 class="product-price">${card[4]}</h2>
+              <p class="product-quantity">Qnt:<input value="1" name="" /></p>
+              <p class="product-remove">
+                <i class="far fa-trash-alt"></i>
+                <span>Remove</span>
+              </p>
+            </div>
       `;
     });
   }
 function removeFromCart(id) {
-    let cart = JSON.parse(localStorage.getItem("cart"));
+    let cart = JSON.parse(localStorage.getItem(".products"));
     let update = cart.filter((item) => item[0].book_id != id);
 
-    localStorage.setItem("cart", JSON.stringify(update));
+    localStorage.setItem(".product", JSON.stringify(update));
   
     getCart();
     getTotal();
@@ -57,7 +60,7 @@ function removeFromCart(id) {
   
   function getTotal() {
     let total = 0;
-    let cart = JSON.parse(localStorage.getItem("cart"));
+    let cart = JSON.parse(localStorage.getItem(".product"));
   
     cart.forEach(
       // (item) => console.log(item[0].price))
@@ -76,7 +79,7 @@ function addToCart(book_id) {
         let books = data.data;
   
         let book = books.filter((book) => book.book_id == book_id);
-        let cart_items = JSON.parse(localStorage.getItem("cart"));
+        let cart_items = JSON.parse(localStorage.getItem(".product"));
         console.log(book);
   
         if (cart_items == null) {
@@ -84,7 +87,7 @@ function addToCart(book_id) {
         }
   
         cart_items.push(book);
-        localStorage.setItem("cart", JSON.stringify(cart_items));
+        localStorage.setItem(".product", JSON.stringify(cart_items));
       });
   }
  
